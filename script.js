5e9d806f11fb81498597936dfbff3a3774e09192
RickWoltheus <rwoltheus@gmail.com>
2016-02-15 12:11:58 +0100

    toegevoegd bestanden githun
    opdracht tabpanelen werkend gemaakt. nu ga ik de opmaak maken.

---
+++ script.js
@@ -0,0 +1,25 @@
+$('.knoppen').each( function() {
+    var dezeKnop = $(this);
+    var actieveKnop = dezeKnop.find('li.actief');
+    var link = actieveKnop.find('a')
+    var actiefPaneel = $(link.attr('href'));
+    
+    dezeKnop.on('click', 'a', function(e) {
+        e.preventDefault();
+        var link = $(this);
+     //   alert(link.attr('href'));
+       var beoogdId = this.hash;
+     //     alert(beoogdId);
+        if(beoogdId && !link.parent().is('.actief')){
+            
+            actiefPaneel.removeClass('actief');
+            actieveKnop.removeClass('actief');
+            
+            actiefPaneel = $(beoogdId).addClass('actief');
+            actieveKnop = link.parent().addClass('actief');
+        }
+    })
+});
+
+// unobrusive
+$('.paneel').css('display','none')
